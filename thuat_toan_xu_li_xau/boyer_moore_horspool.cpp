1a8faa44448d69f8bd7b4ba815c96ac343750934
Tien Minh <tienminh@Tiens-MacBook-Air.local>
2023-06-09 04:00:35 +0700

    Algorithm solve string

---
+++ thuat_toan_xu_li_xau/boyer_moore_horspool.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int char_in_string(char c, string P)
+{
+    for (int i = 0; i < P.size(); i++)
+    {
+        if (c == P[i])
+            return i;
+    }
+
+    return -1;
+}
+
+int boyer_moore_horspool(string T, string P)
+{
+    int i = P.size(), v = P.size();
+    int cnt = 0;
+    while (i <= T.size())
+    {
+        int x = v - 1, j = i - 1;
+        while (T[j] == P[x] && x > -1)
+        {
+            x--;
+            j--;
+        }
+        if (x < 0)
+        {
+            cnt++;
+            i += v;
+        }
+        else
+        {
+            int k = char_in_string(T[j], P);
+            if (k < 0)
+            {
+                i += v;
+            }
+            else
+            {
+                i += v - x;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    string T = "ABCDEAMEFDHKEVDEFGHK",
+           P = "DEFGHK";
+
+    cout << "T = " << T << endl;
+    cout << "P = " << P << endl;
+    cout << "So lan P xuat hien trong T: " << boyer_moore_horspool(T, P) << endl;
+
+    return 0;
+}
